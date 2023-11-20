4f583d15b5951810cfa5ba88cb5a2c4015d053ab
Batdarg <batdarg@gmail.com>
2023-11-20 14:05:47 -0600

    Add Files Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+//Usando el componente Cards
+import Cards from "./Components/Cards";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const App = () => {
+  return <Cards />;
+};
+
+export default App;
